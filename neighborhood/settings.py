6c49ea60828f9b7e0a10e253f2f9207ca10fa8c4
victoria mwangi <victoriamwangi021@gmail.com>
2022-06-15 14:56:46 +0300

    base.html

--- neighborhood/settings.py
+++ neighborhood/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'neighborapp'
+    'neighborapp',
+    'bootstrap5'
 ]
 
 MIDDLEWARE = [
