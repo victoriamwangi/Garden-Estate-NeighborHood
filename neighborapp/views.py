b534ba33dd31a8b7909c8d9f4f237653e7b99b3e
victoria mwangi <victoriamwangi021@gmail.com>
2022-06-15 17:11:59 +0300

    add registration form

--- neighborapp/views.py
+++ neighborapp/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html') 
+
+# @login_required(login_url= '/accounts/login/')
+
