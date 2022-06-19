3a25323342ba442200b3147bb2f9704bb3619d48
victoria mwangi <victoriamwangi021@gmail.com>
2022-06-19 16:59:49 +0300

    deploy to heroku

--- neighborhood/settings.py
+++ neighborhood/settings.py
@@ -148,6 +148,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
